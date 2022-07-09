b86f4cf69a08b62c805cb75e9f0cbe2d8758cad0
lgwk42 <76110334+lgwk42@users.noreply.github.com>
2022-07-09 16:42:16 +0900

    Add files via upload

---
+++ script.py
@@ -0,0 +1,39 @@
+import webbrowser
+
+# HTML 수정 함수
+def write(name, desc):
+  Element(name).element.innerText = desc
+
+# 하단 버튼 링크 연결 함수
+def button(*args):
+  link = "https://www.youtube.com/" # https:// 꼭 붙여야 연결됩니다!
+  webbrowser.open(link)
+
+# 배경 색깔 설정 함수
+def background(color):
+  for i in range(0, 2):
+    write("g" + str(i), color[i])
+
+def information(info):
+  key = list(info.keys())
+  for i in range(0, 4):
+    write("a" + str(i), key[i])
+    write("b" + str(i), info[key[i]])
+
+# 배경 색깔 설정
+colors = ["white", "black"]
+background(colors)
+
+# 이름과 설명, 버튼에 들어갈 글 설정
+write("name", "김건우")
+write("description", "대구동부중학교")
+write("button", "확인")
+
+# 상세설명에 들어갈 제목과 글 설정
+informations = {
+  "나이": "17살",
+  "성별": "남",
+  "취미": "게임, 마술",
+  "MBTI": "ISTP"
+}
+information(informations)
